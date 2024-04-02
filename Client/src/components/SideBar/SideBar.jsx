04c776832ad70c4fc683cbf9837cce227c3dcd08
AryanSingh645 <singhjee30476@gmail.com>
2024-04-02 13:26:41 +0530

    starting

--- Client/src/components/SideBar/SideBar.jsx
+++ Client/src/components/SideBar/SideBar.jsx
@@ -36,7 +36,7 @@ function SideBar() {
               className='flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800'
               >
                 <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-home"></i></span>
-                <span className="text-sm font-medium">{item.name}</span>
+                <span className="text-sm font-medium">aryan{item.name}</span>
               </Link>
             </li>
           ))}
